2c4313be006f0cdbfe96105f7ba47f13c7e51d66
Jorge Velasquez <jluisv16@gmail.com>
2025-02-18 22:18:49 +0100

    Primer commit del proyecto

---
+++ src/main/java/com/inditex/productprice/api/ProductpriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.productprice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductpriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductpriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/productprice/api/ProductpriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.productprice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductpriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
